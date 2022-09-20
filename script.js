e61b2f81557b3f205c28fb0381217ba51a48ec99
Papaia <43409674+papaia@users.noreply.github.com>
2022-09-20 21:38:29 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const D = 20;
+const CHANCE = 0.03;
+
+const btn = document.getElementById("no");
+btn.addEventListener("mouseover", teleport);
+
+const random = (l, h) => l + Math.round(Math.random() * (h - l));
+
+const getNew = (wW, eW, pos) => {
+  const newPos = eW + Math.round(Math.random() * (wW - eW - eW))
+  if (newPos > pos) return Math.max(pos + D, newPos);
+  return Math.min(pos - D, newPos);
+};
+
+function teleport() {
+  const { top, left, width, height } = btn.getBoundingClientRect();
+  const newLeft = getNew(window.innerWidth - D, width, left);
+  const newTop = random(window.innerHeight - D, height, top);
+  
+  delete btn.style.transform;
+  btn.style.left = newLeft + 'px';
+  btn.style.top = newTop + 'px';
+  
+  if (Math.random() <= CHANCE) {
+    btn.removeEventListener("mouseover", teleport);
+    btn.addEventListener("click", loser);
+    btn.innerHTML = "Yes!";
+  }
+}
+
+function loser() {
+  const h1 = document.getElementById('h');
+  alert("I KNOW!!!!! HAHAHAHHA");
+  setTimeout(() => window.location = 'https://youtu.be/dQw4w9WgXcQ', 3000);
+}
